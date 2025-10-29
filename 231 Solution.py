982514f95703096c2201879dbd94440571d38932
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-28 22:31:46 -0400

    Implement method to check if number is power of two

---
+++ 231 Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+      if n <= 0:
+          return False
+      i = 0
+      while 2**i <= n:
+        if 2**i == n:
+            return True
+        i += 1
+      return False
+        
