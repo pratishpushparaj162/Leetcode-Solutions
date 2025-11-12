4cdde891376faa101327ef4807ab7e7ddb390c85
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-11-11 23:09:14 -0500

    Create 516 Solution.java

---
+++ 516 Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String revStr = new StringBuilder(s).reverse().toString();
+        int m = s.length();
+        int n = revStr.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == revStr.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
