6753bddfcc7a643bd04a5cb16962231bc85d9bfd
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-11-16 22:48:55 -0500

    Create 589 Solution.py

---
+++ 589 Solution.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(node):
+            if not node:
+                return
+            
+            res.append(node.val)
+            for no in node.children:
+                dfs(no)
+        
+        dfs(root)
+        return res
